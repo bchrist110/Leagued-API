b7be7c3c2e038405b14243ff4e71a076fc33d093
Brandon Christianson <b.christianson110@gmail.com>
2021-02-08 15:06:28 -0800

    first commit

---
+++ sql-scripts/seed.leagues.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+INSERT INTO leagues (name)
+VALUES
+    ('Tater Tots (8-10)'),
+    ('Flyers (11-12)'),
+    ('Trappers (13-15)'),
+    ('Killas (16-18)');
+
+COMMIT;
---
+++ sql-scripts/seed.teams.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+INSERT INTO teams (name)
+VALUES
+    (1, 'Supers'),
+    (1, 'Importants'),
+    (1, 'Spangleys'),
+    (1, 'Terrors'),
+    (2, 'Mirrors'),
+    (2, 'Tables'),
+    (2, 'Times'),
+    (2, 'Kitties'),
+    (3, 'Bobs'),
+    (3, 'Sleeves'),
+    (3, 'Bears'),
+    (3, 'Bad Boys'),
+    (4, 'Trippys'),
+    (4, 'Snows'),
+    (4, 'Buyers'),
+    (4, 'Consumers');
+
+COMMIT;
